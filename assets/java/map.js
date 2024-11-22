ea96bfdefab4b84ce0403efebfe9bfc20277986e
Pepe066 <pietrotisci01@gmail.com>
2024-11-22 16:56:22 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ assets/java/map.js
@@ -0,0 +1,13 @@
+function showList() {
+	const listContainer = document.getElementById('list-container');
+	const icon = document.getElementById('icon');
+  
+	// Toggle display between 'none' and 'block'
+	if (listContainer.style.display === 'none' || listContainer.style.display === '') {
+	  listContainer.style.display = 'block';
+	  icon.style.transform = 'rotate(180deg)'; // Rotate icon by 180°
+	} else {
+	  listContainer.style.display = 'none';
+	  icon.style.transform = 'rotate(0deg)'; // Reset icon rotation
+	}
+  }
