a96d320486a5ad914352c310a56e023e9868344f
Lucrezia Pograri <ateossg5@gmail.com>
2024-12-02 23:33:37 +0100

    corretto spinner

--- assets/java/common.js
+++ assets/java/common.js
@@ -1,9 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Quando la pagina è completamente caricata, rimuovi lo spinner e mostra il contenuto
-    document.body.classList.add('loaded');
-    
-    const content = document.querySelector('.content');
-    if (content) {
-        content.style.display = 'block'; // Mostra il contenuto
+window.addEventListener('load', function () {
+    // Timeout di sicurezza per rimuovere lo spinner dopo 10 secondi
+    const timeout = setTimeout(function () {
+        if (!document.readyState === 'complete') {
+            const loadingScreen = document.getElementById('loadingScreen');
+            loadingScreen.classList.add('hidden');
+        }
+    }, 10000); // Timeout di 10 secondi
+
+    // Rimuovi lo spinner quando la pagina è completamente caricata
+    if (document.readyState === 'complete') {
+        const loadingScreen = document.getElementById('loadingScreen');
+        loadingScreen.classList.add('hidden');
     }
+    // Pulisci il timeout di sicurezza se il caricamento è veloce
+    clearTimeout(timeout);
+
 });
