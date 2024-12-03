379a07920f5eda5ea6e62dac50466ea8a11bbf7e
Lucrezia Pograri <ateossg5@gmail.com>
2024-12-03 13:55:50 +0100

    documentazione

--- assets/java/common.js
+++ assets/java/common.js
@@ -1,18 +1,4 @@
 window.addEventListener('load', function () {
-    // Timeout di sicurezza per rimuovere lo spinner dopo 10 secondi
-    const timeout = setTimeout(function () {
-        if (!document.readyState === 'complete') {
-            const loadingScreen = document.getElementById('loadingScreen');
-            loadingScreen.classList.add('hidden');
-        }
-    }, 10000); // Timeout di 10 secondi
-
-    // Rimuovi lo spinner quando la pagina è completamente caricata
-    if (document.readyState === 'complete') {
-        const loadingScreen = document.getElementById('loadingScreen');
-        loadingScreen.classList.add('hidden');
-    }
-    // Pulisci il timeout di sicurezza se il caricamento è veloce
-    clearTimeout(timeout);
-
+    // Aggiungi la classe 'loaded' al body per nascondere lo spinner
+    document.body.classList.add('loaded');
 });
