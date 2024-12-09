72396478a9f7cab86e027be296cae231ea965721
Liber-R <romolo.66@hotmail.it>
2024-12-09 18:48:56 +0100

    aggiustato menu burger timeline

--- assets/java/timeline.js
+++ assets/java/timeline.js
@@ -1,3 +1,20 @@
+// Funzione per attivare e disattivare con un click il menu hamburger, quando si attiva il menu hamburger si opacizza lo sfondo
+function toggleCurtain() {
+	let menu_links = document.getElementById('menu-links');
+	let toggleButton = document.getElementById('toggleButton');
+	let opaque = document.getElementById('opaque-page');
+	if (menu_links.style.display === "grid") {
+		menu_links.style.display = "none";
+		opaque.classList.remove("active");
+		document.body.style.overflow = "";
+	} else {
+		menu_links.style.display = "grid";
+		opaque.classList.add("active");
+		document.body.style.overflow = "hidden";	
+	}
+	toggleButton.classList.toggle('x');
+  }
+
 // Funzione per avere un pulsante che rimandi all'inizio della pagina
 window.addEventListener('scroll', function() { // Necessario per controllare il valore dello scroll della pagina 
 	let upButton = document.getElementById("upButton");
