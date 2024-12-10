310bfc2585a3d2b71abbc6696f0af778d472b756
Lucrezia Pograri <ateossg5@gmail.com>
2024-12-10 13:12:47 +0100

    perm navbar

--- assets/java/items.js
+++ assets/java/items.js
@@ -81,6 +81,21 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
     // fine FUNZIONI PER SELECTED CUSTOMIZATO
+
+    // fissare linea link menu navbar in base a selezione
+	// Ottieni l'URL corrente
+    const currentUrl = window.location.href;
+
+    // Seleziona tutti i link con la classe nav-link
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+        // Controlla se l'href del link corrisponde all'URL corrente
+        if (link.href === currentUrl) {
+            // Aggiungi la classe 'active' al link corrispondente
+            link.classList.add('perm');
+        }
+    });
     
 });
 
