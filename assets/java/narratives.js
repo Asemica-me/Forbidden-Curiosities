6834124bcd42060ec12677d891e0acf5e94e5898
Pepe066 <pietrotisci01@gmail.com>
2024-12-19 11:47:00 +0100

    clean up

--- assets/java/narratives.js
+++ assets/java/narratives.js
@@ -110,18 +110,3 @@ let NarSum = {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Ottieni l'URL corrente
-    const currentUrl = window.location.href;
-
-    // Seleziona tutti i link con la classe nav-link
-    const navLinks = document.querySelectorAll('.nav-link');
-
-    navLinks.forEach(link => {
-        // Controlla se l'href del link corrisponde all'URL corrente
-        if (link.href === currentUrl) {
-            // Aggiungi la classe 'active' al link corrispondente
-            link.classList.add('perm');
-        }
-    });
-});
